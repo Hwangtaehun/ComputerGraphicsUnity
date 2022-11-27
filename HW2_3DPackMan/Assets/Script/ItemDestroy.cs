e6eccf8e841304ffd5e252f725c98c5debe7a419
Hwangtaehun <r283@naver.com>
2022-11-27 21:57:28 +0900

    보고서 작성완료

--- HW2_3DPackMan/Assets/Script/ItemDestroy.cs
+++ HW2_3DPackMan/Assets/Script/ItemDestroy.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class ItemDestroy : MonoBehaviour
 {
-    //private float currentPosition;
-    //public float moveSpeed = 10.0f;
+    private float currentPosition;
+    public float moveSpeed = 10.0f;
 
     void Start()
     {
         Destroy(gameObject, 5.0f);
-        //currentPosition = transform.position.y;
+        currentPosition = transform.position.y;
     }
     void Update()
     {
-        //currentPosition -= Time.deltaTime * moveSpeed;
-        //transform.position = new Vector3(transform.position.x, currentPosition, transform.position.z);
+        currentPosition -= Time.deltaTime * moveSpeed;
+        transform.position = new Vector3(transform.position.x, currentPosition, transform.position.z);
 
-        //if (transform.position.y <= 0.5f)
-        //{
-        //    moveSpeed = 0.0f;
-        //}
+        if (transform.position.y <= 0.5f)
+        {
+            moveSpeed = 0.0f;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
